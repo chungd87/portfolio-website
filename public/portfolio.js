7bec9fd951cb8a1d08ed4866f7651c83c8b02a26
chungd87 <chungd87@gmail.com>
2021-12-26 08:47:38 -0800

    layout fix
    fixed layout cut off on iphones

--- public/portfolio.js
+++ public/portfolio.js
@@ -12,16 +12,16 @@ portfolio_link.addEventListener("click", function (event) {
 
     screen_content.innerHTML = '\
     <h1 id = "contentStyle">Portfolio</h1><br>\
-    <div class = "container" style = "overflow-x: scroll; min-width: 900px;">\
+    <div class = "container" style = "overflow-x: auto; min-width: 900px;">\
         <div class = "row flex-nowrap" style="min-height: 400px; max-height: 800px; min-width: 900px; overflow-x: auto;">\
-            <div class="col-3">\
-                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2;">\
+            <div class="col-4">\
+                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2;  height: 575px;">\
                     <a target="_blank" href="https://drive.google.com/file/d/1VffSXJHp-0ms7LzL83DH0zxcMvejkF0f/view?usp=sharing">\
-                        <img style = "max-width: 274px; max-height: 155px;" class="card-img-top" src="./public/project1.png" alt="Card image cap"></a>\
+                        <img style = "width: 100%; max-height: 155px;" class="card-img-top" src="./public/project1.png" alt="Card image cap"></a>\
                     <div class="card-body">\
-                        <h5 class="card-title">D&D Character Sheet App</h5><br>\
+                        <h5 class="card-title" style = "font-weight: bold;">D&D Character Sheet App</h5><br>\
                         <p class="card-text">Windows app made for streamlining the in person D&D player experience. Writes and loads data from .json and .csv into the User Interface made with tkinter.\
-                        Simple local database interaction with SQLite.</p>\
+                        Simple local database interaction with SQLite for random fantasy name generation.</p><br>\
                         <i id ="stackP" class="devicon-python-plain-wordmark colored"></i>\
                         <img src="./icons/csv.png" height = "30" style = "margin-top: -35px; margin-left: 5px;">\
                         <img src="./icons/json.png" height = "28" style = "margin-top: -35px; margin-left: 5px;">\
@@ -32,14 +32,14 @@ portfolio_link.addEventListener("click", function (event) {
                     </div>\
                 </div>\
             </div >\
-            <div class="col-3">\
-                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2;">\
+            <div class="col-4">\
+                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2; height: 575px;">\
                     <a target="_blank" href="https://drive.google.com/file/d/1_glZsw1_kuUWNIsuoJKjGDzGprRcNwit/view?usp=sharing">\
-                        <img style = "max-width: 274px; max-height: 155px;" class="card-img-top" src="./public/project2.png" alt="Card image cap"></a>\
+                        <img style = "width: 100%; max-height: 155px;" class="card-img-top" src="./public/project2.png" alt="Card image cap"></a>\
                     <div class="card-body">\
-                        <h5 class="card-title">Movie Studio Database WebApp</h5>\
+                        <h5 class="card-title" style = "font-weight: bold;">Movie Studio Database WebApp</h5><br>\
                         <p style = "font-size: 13.5px" class="card-text">Designed for a fictional movie studio, this back-end focused web app tracks their budget and entities.\
-                        Database interaction with MySQL, display of data with Express. Javascript and Node. Collaborative partner project (Danny Chung and Hailee Hibray).</p>\
+                        Database interaction with MySQL, display of data with Express. Javascript and Node. Collaborative partner project (Danny Chung and Hailee Hibray).</p><br><br>\
                         <i id ="stackH" class="devicon-html5-plain-wordmark colored"></i>\
                         <i id ="stackCSS" class="devicon-css3-plain-wordmark colored"></i>\
                         <i id ="stackJS" class="devicon-javascript-plain colored"></i>\
@@ -54,9 +54,26 @@ portfolio_link.addEventListener("click", function (event) {
                     </div>\
                 </div>\
             </div >\
-            <div class="col-3">\
-                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2;">\
-                        <img style = "max-width: 274px; max-height: 155px;" class="card-img-top" src="./public/soon.png" alt="Card image cap">\
+            <div class="col-4">\
+            <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2; height: 575px;">\
+                    <img style = "width: 100%; max-height: 155px;" class="card-img-top" src="./public/portfvidstillsmall.bmp" alt="Card image cap">\
+                <div class="card-body">\
+                    <h5 class="card-title" style = "font-weight: bold;">Portfolio Website Code Repository</h5><br>\
+                    <p class="card-text">Responsive front-end web development with BootStrap. DOM manipulation with JavaScript and styling of HTML markup with CSS.</p><br><br><br><br>\
+                    <i id ="stackH" class="devicon-html5-plain-wordmark colored"></i>\
+                    <i id ="stackCSS" class="devicon-css3-plain-wordmark colored"></i>\
+                    <i id ="stackJS" class="devicon-javascript-plain colored"></i>\
+                    <i id ="stackBS" class="devicon-bootstrap-plain-wordmark colored"></i>\
+                </div>\
+                <div class="card-footer">\
+                <a target="_blank" href="https://github.com/chungd87/portfolio-website">\
+                <small class="text-muted"><i id = "Gicon" class="devicon-github-original-wordmark colored"></i></small></a>\
+                </div>\
+            </div>\
+        </div >\
+            <div class="col-4">\
+                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2; height: 575px;">\
+                        <img style = "width: 100%; max-height: 155px;" class="card-img-top" src="./public/soon.png" alt="Card image cap">\
                     <div class="card-body">\
                         <h5 class="card-title"></h5>\
                         <p class="card-text"></p>\
@@ -64,23 +81,6 @@ portfolio_link.addEventListener("click", function (event) {
                     </div>\
                     <div class="card-footer">\
                         <small class="text-muted"></small>\
-                    </div>\
-                </div>\
-            </div >\
-            <div class="col-3">\
-                <div class="card card-block" style = "margin: 5px; background-color: #f2f2f2;">\
-                        <img style = "max-width: 274px; max-height: 155px;" class="card-img-top" src="./public/portfvidstillsmall.bmp" alt="Card image cap">\
-                    <div class="card-body">\
-                        <h5 class="card-title">Portfolio Website Code Repository</h5><br>\
-                        <p class="card-text">Responsive front-end web development with BootStrap. DOM manipulation with JavaScript and styling of HTML markup with CSS.</p>\
-                        <i id ="stackH" class="devicon-html5-plain-wordmark colored"></i>\
-                        <i id ="stackCSS" class="devicon-css3-plain-wordmark colored"></i>\
-                        <i id ="stackJS" class="devicon-javascript-plain colored"></i>\
-                        <i id ="stackBS" class="devicon-bootstrap-plain-wordmark colored"></i>\
-                    </div>\
-                    <div class="card-footer">\
-                    <a target="_blank" href="https://github.com/chungd87/portfolio-website">\
-                    <small class="text-muted"><i id = "Gicon" class="devicon-github-original-wordmark colored"></i></small></a>\
                     </div>\
                 </div>\
             </div >\
